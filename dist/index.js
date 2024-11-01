321259f3494b06154a7a1f516381a8ae9d1fa121
Eunjung-practice <sogoal17@gmail.com>
2024-11-01 18:05:42 -0500

    cl

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("hello world");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const User_1 = __importDefault(require("./User"));
+const user1 = new User_1.default("asdasdasdasd@gmail.com", "1234", "haha", 12);
+console.log(user1.getEmail());
+// user2 instance
+const user2 = new User_1.default("aaa@gmail.com", "1234", "hoho", 12);
+console.log(user2.getAge());
+console.log(user2.getPassword());
+user2.changePassword("5678");
+console.log(user2.getPassword());
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello world")
+import User from './User'
+const user1 = new User("asdasdasdasd@gmail.com", "1234", "haha", 12)
+console.log(user1.getEmail())
+
+// user2 instance
+const user2 = new User("aaa@gmail.com", "1234", "hoho", 12)
+console.log(user2.getAge())
+console.log(user2.getPassword())
+user2.changePassword("5678")
+console.log(user2.getPassword())
