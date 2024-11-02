ee6373389fc9389da527d538c38e9b9eeaf932e8
Eunjung-practice <sogoal17@gmail.com>
2024-11-01 20:06:44 -0500

    start

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,39 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const User_1 = __importDefault(require("./User"));
-const user1 = new User_1.default("asdasdasdasd@gmail.com", "1234", "haha", 12);
-console.log(user1.getEmail());
-// user2 instance
-const user2 = new User_1.default("aaa@gmail.com", "1234", "hoho", 12);
-console.log(user2.getAge());
-console.log(user2.getPassword());
-user2.changePassword("5678");
-console.log(user2.getPassword());
+const app = () => __awaiter(void 0, void 0, void 0, function* () {
+    /*
+    const inputName1 = await input("What is your name?: ")
+    const nameFilePath1 = path.join(__dirname, "../data/name.txt")
+    fs.appendFileSync(nameFilePath1, `${inputName1}\n`)
+
+    const inputName2 = await input("What is your name?: ")
+    const nameFilePath2 = path.join(__dirname, "../data/name.txt")
+    fs.appendFileSync(nameFilePath2, `${inputName2}\n`)
+
+    const inputName3 = await input("What is your name?: ")
+    const nameFilePath3 = path.join(__dirname, "../data/name.txt")
+    fs.appendFileSync(nameFilePath3, `${inputName3}\n`)
+
+    const inputName4 = await input("What is your name?: ")
+    const nameFilePath4 = path.join(__dirname, "../data/name.txt")
+    fs.appendFileSync(nameFilePath4, `${inputName4}\n`)
+
+    fs.readFileSync()*/
+    //const x1 = 1
+    //console.log(x1)
+    //const x2 = 1
+    //console.log(x2)
+    const x = 1;
+    console.log(x);
+    console.log(x);
+});
+app();
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
+import { input } from './input'
 import User from './User'
-const user1 = new User("asdasdasdasd@gmail.com", "1234", "haha", 12)
-console.log(user1.getEmail())
+import fs from 'node:fs'
+import path from 'node:path'
 
-// user2 instance
-const user2 = new User("aaa@gmail.com", "1234", "hoho", 12)
-console.log(user2.getAge())
-console.log(user2.getPassword())
-user2.changePassword("5678")
-console.log(user2.getPassword())
+const app = async () => {
+    /*
+    const inputName1 = await input("What is your name?: ")
+    const nameFilePath1 = path.join(__dirname, "../data/name.txt")
+    fs.appendFileSync(nameFilePath1, `${inputName1}\n`)
+
+    const inputName2 = await input("What is your name?: ")
+    const nameFilePath2 = path.join(__dirname, "../data/name.txt")
+    fs.appendFileSync(nameFilePath2, `${inputName2}\n`)
+
+    const inputName3 = await input("What is your name?: ")
+    const nameFilePath3 = path.join(__dirname, "../data/name.txt")
+    fs.appendFileSync(nameFilePath3, `${inputName3}\n`)
+
+    const inputName4 = await input("What is your name?: ")
+    const nameFilePath4 = path.join(__dirname, "../data/name.txt")
+    fs.appendFileSync(nameFilePath4, `${inputName4}\n`)
+
+    fs.readFileSync()*/
+    //const x1 = 1
+    //console.log(x1)
+    //const x2 = 1
+    //console.log(x2)
+    const x = 1
+    console.log(x)
+    console.log(x)
+}
+app()
